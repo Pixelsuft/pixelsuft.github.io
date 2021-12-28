b160220ac6644958443cd48a1432721e052c8dc8
Pixelsuft <yuskivl07@mail.ru>
2021-12-28 20:41:26 +0700

    updates

--- js/index.js
+++ js/index.js
@@ -4,14 +4,17 @@ window.addEventListener('contextmenu', function(e) {
 
 
 function main() {
-  const topnav = document.getElementById('pixelTop');
-  const tops = topnav.getElementsByClassName('link');
-  for (var i = 0; i < tops.length; i++) {
-    const current_top = tops[i];
-    current_top.addEventListener('click', function() {
-      location.href = current_top.getAttribute('href');
+  const copyright = document.getElementById('copyright');
+  const links = document.getElementsByClassName('link');
+
+  for (var i = 0; i < links.length; i++) {
+    const current_link = links[i];
+    current_link.addEventListener('click', function() {
+      location.href = current_link.getAttribute('href');
     });
   }
+
+  copyright.textContent += ' ' + new Date().getFullYear();
 }
 
 window.onload = main;
