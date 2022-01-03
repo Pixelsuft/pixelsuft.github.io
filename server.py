917dd2dd4178394cd1244bfe3781c046e34b7fda
Pixelsuft <yuskivl07@mail.ru>
2022-01-03 15:49:35 +0700

    updates

--- server.py
+++ server.py
@@ -23,27 +23,27 @@ def get_path_from_url(url_: str) -> str:
 
 
 def file_exists(fn: str) -> bool:
-    return os.access(fn, os.F_OK)
+    return os.access(fn, os.F_OK) and not os.path.isdir(fn)
+
+
+def try_render(fn: str) -> None:
+    if file_exists(fn):
+        return fast_read(fn), 200
+    return error404()
 
 
 @app.errorhandler(404)
 def get_page(*args, **kwargs) -> any:
     url = flask.request.url
     path = get_path_from_url(url)
+    if not path:
+        return try_render('index.html')
     ext = url.split('.')[-1].lower().strip()
     if ext in ('html', 'htm'):
-        if not file_exists(path):
-            return error404()
-        return fast_read(path), 200
-    if file_exists(path) and not os.path.isdir(path):
+        return try_render(path)
+    if file_exists(path):
         return app.send_static_file(path), 200
-    if url.endswith('/'):
-        url = url[:-1]
-    test_path = get_path_from_url(url + '/index.html')
-    print(url, path, test_path)
-    if file_exists(test_path):
-        return fast_read(test_path), 200
-    return error404()
+    return try_render(get_path_from_url(url + 'index.html'))
 
 
 if __name__ == '__main__':
